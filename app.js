ef179e084e0c308479d4169d95a3220e6b10a4d9
haithem satoutah <satoutahhaithem@gmail.com>
2023-03-11 11:38:07 +0100

    learn how use layer caching on docker

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ app.use(cors())
 app.get('/', (req, res) => {
   res.json([
     {
-      "id":"1",
+      "id":"5",
       "title":"Book Review: The Bear & The Nightingale"
     },
     {
-      "id":"2",
+      "id":"6",
       "title":"Game Review: Pokemon Brillian Diamond"
     },
     {
